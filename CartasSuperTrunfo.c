8d5ae6928c533aa1ca8392fe8290a5fb056190e0
shimon-net <164230386+shimon-net@users.noreply.github.com>
2025-04-29 19:52:36 -0400

    Update CartasSuperTrunfo.c
    Adicionado os calculos de Densidade Populacional e PIB Per Capita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,13 +8,13 @@
 
 int main() {
     int populacao, num_turis;
-    float area, pib;
+    float area, pib,denpopu,pibpercap;
     char cod_carta[5];
     char cidade[30];
     char estado[30];
 
     int populacao2, num_turis2;
-    float area2, pib2;
+    float area2, pib2,denpopu2,pibpercap2;
     char cod_carta2[5];
     char cidade2[30];
     char estado2[30];
@@ -52,6 +52,14 @@ int main() {
     printf("Digite Numeros dos Pontos Turisticos: ");
     scanf("%d",&num_turis2);
     system("cls");
+
+//calculando
+    denpopu=(populacao/area);
+    pibpercap=(pib/populacao);
+    denpopu2=(populacao2/area2);
+    pibpercap2=(pib2/populacao2);
+
+
 // exibindo os dados
 
     printf("CARTA 1 \n");
@@ -62,6 +70,8 @@ int main() {
     printf("Area: %f Km2 \n",area);
     printf("PIB: %f \n",pib);
     printf("Numeros de Pontos Turisticos: %d \n",num_turis);
+    printf("Dencidade Populacional: %f hab/km2 \n",denpopu);
+    printf("PIB Per Capita: %f  Reais \n",pibpercap);
     printf("\n");
     printf("\n");
     printf("CARTA 2 \n");
@@ -72,6 +82,8 @@ int main() {
     printf("Area: %f Km2 \n",area2);
     printf("PIB: %f \n",pib2);
     printf("Numeros de Pontos Turisticos: %d \n",num_turis2);
+    printf("Dencidade Populacional: %f  hab/km2 \n",denpopu2);
+    printf("PIB Per Capita: %f  Reais \n",pibpercap2);
 
-    return 0;
+ return 0;
 }
